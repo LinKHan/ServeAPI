6331448439b4c3d1f52a94b349926be79f741119
LinKHan <afzalkhan2357@gmail.com>
2020-11-03 17:55:29 -0500

    auditing feature enabled in db

--- src/main/java/com/ak/restAPIBoot/RestApiBootApplication.java
+++ src/main/java/com/ak/restAPIBoot/RestApiBootApplication.java
@@ -2,8 +2,10 @@ package com.ak.restAPIBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RestApiBootApplication {
 
 	public static void main(String[] args) {
