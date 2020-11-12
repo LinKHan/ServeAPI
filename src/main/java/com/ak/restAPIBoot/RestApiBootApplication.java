7fc0ef1a04ee9c43d02e8b0a6bb0cc5eb5c75d87
LinKHan <afzalkhan2357@gmail.com>
2020-11-11 22:04:45 -0500

    servelet

--- src/main/java/com/ak/restAPIBoot/RestApiBootApplication.java
+++ src/main/java/com/ak/restAPIBoot/RestApiBootApplication.java
@@ -2,12 +2,12 @@ package com.ak.restAPIBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class RestApiBootApplication {
-
+public class RestApiBootApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiBootApplication.class, args);
 	}
